5b166a6da45edb99d3c16202b3e863f08cc39d10
DAUDA SHERIFFDEEN DAYO <s.dayo36@yahoo.com>
2022-09-08 23:45:30 -0700

    put put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0  
+ *  main - Entry point
+ *  
+ *  Return: Always 0  
 */
 
 int main(void)
