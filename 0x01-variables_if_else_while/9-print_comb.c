8fd1eb997e44799b3c105d2bc3b27ec98fe44f3c
DAUDA SHERIFFDEEN DAYO <s.dayo36@yahoo.com>
2022-09-12 18:31:52 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-		int c;
+	int c;
 
-			for (c = 48; c <= 57; c++)
-					{
-								putchar(c);
-										if (c != 57)
-													{
+		for (c = 48; c <= 57; c++)
+{
+	putchar(c);
+		if (c != 57)
+{
 																	putchar(',');
-																				putchar(' ');
-																						}
-											}
-				putchar('\n');
-					return (0);
+																	putchar(' ')								
+}
+
+	putchar('\n');
+	return (0);
 }
