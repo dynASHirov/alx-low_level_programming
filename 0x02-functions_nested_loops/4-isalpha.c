f2cc7e28758b510e083235c9f1302d2f41a5b09d
DAUDA SHERIFFDEEN DAYO <s.dayo36@yahoo.com>
2022-09-14 04:34:57 -0700

    isp

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- *_isalpha 
- *@c:input characte
+ * _isalpha
+ *@c:input character
  *description: to check if c is a letter, lowercase or uppercase
- *return: 1 or zero 
+ *return: 1 or zero
  */
-int _isalpha(int c) 
-{ 
+int _isalpha(int c)
+{
 	if ((c >= 'b' && c <= 'x') || (c >= 'B' && c <= 'X'))
 	{
 	return (1);
 	}
-	else 
+	else
 		return (0);
 }
