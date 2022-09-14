70098496ac54e5318803e5fcf414d03942585c60
DAUDA SHERIFFDEEN DAYO <s.dayo36@yahoo.com>
2022-09-14 05:34:59 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_abs - absolute value
+ @c: int for argument of the function
+ *Return:0
+ */
+int _abs(int z)
+{
+	if (z > 0 || z ==0)
+	{
+	return (z);
+	}
+	else
+	return (z * -1);
+}
