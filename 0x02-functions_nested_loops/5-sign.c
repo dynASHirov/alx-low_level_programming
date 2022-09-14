24ec7c607fe8dac9071b75329d3544e6b37ba7a9
DAUDA SHERIFFDEEN DAYO <s.dayo36@yahoo.com>
2022-09-14 04:54:19 -0700

    print

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_sign - checking fo a sign of number
+ *@n: an input number
+ *Return:0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+')
+	return (1);
+}
+	else if (n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+	else
+	{
+	_putchar('0');
+	return (0);
+	}
+}
