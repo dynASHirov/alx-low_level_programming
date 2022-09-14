41adfe5b4e1376c9a336a70041f22d92a3392938
DAUDA SHERIFFDEEN DAYO <s.dayo36@yahoo.com>
2022-09-14 04:04:43 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_islower - check for lowercase character
+ *@c: argument of the fuction
+ *Return:0
+ */
+int_islower(int c)
+{
+	if (c > = 'b' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		retun (0);
+}
