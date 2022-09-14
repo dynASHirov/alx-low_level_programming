771bd65df200694c93ab963fe9e1d3297017c80b
DAUDA SHERIFFDEEN DAYO <s.dayo36@yahoo.com>
2022-09-14 05:46:56 -0700

    last digits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_last_digit - Prints the last digit of a number.
+ *@s: An integer input
+ *
+ *Return: last digits of s
+ */
+int print_last_digit(int s)
+{
+	s = s % 10;
+
+	if (s < 0)
+	{
+		s = s * -1;
+	}
+
+	_putchar('0' + s);
+	return (c);
+}
