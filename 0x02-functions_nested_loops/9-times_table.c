2b1a61040cdafc6ba16c0288ed0431297f227a1b
DAUDA SHERIFFDEEN DAY <s.dayo36@yahoo.com>
2022-09-15 02:32:48 +0200

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,12 @@ void times_table(void)
 			{
 				u = z % 10;
 				d = (z - u) / 10;
-				_putchar(44+ '0');
+				
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+
 			}
 			else
 			{
