825f1fe9ada6fd1ad8fc09858a05c128a6ff81cb
DAUDA SHERIFFDEEN DAY <s.dayo36@yahoo.com>
2022-09-15 02:42:07 +0200

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,23 @@
 #include "main.h"
 #include <stdio.h>
+* print_to_98 - from n to 98
+ * @n: integer
+ *
+ * Return: this function print from n to 98
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
 
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
