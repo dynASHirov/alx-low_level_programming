35198f0a4ade081ed2c879d1db724605604ce624
DAUDA SHERIFFDEEN DAY <s.dayo36@yahoo.com>
2022-09-16 02:39:33 +0200

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,25 +1,43 @@
-#include <main.h>
+#include <stdio.h>
 
 /**
- * main - Entry piont
- * Return: Always 0
- */
+*main - program that prints either number
+*or fizz or buzz or fizzBuzz
+*
+*
+*Return: returns 0
+*/
+
 int main(void)
 {
-	int i = 1;
+	int num = 1;
 
-	for (; i < 100 ; i++)
+	while (num++ < 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
 			printf("FizzBuzz ");
-		else if (i % 3 == 0)
+		}
+		else if ((num % 3) == 0)
+		{
 			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			if (num != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
 		else
-			printf("%d ", i);
+		{
+			printf("%d ", num);
+		}
 	}
-	printf("Buzz\n");
+	printf("\n");
 
 	return (0);
-}
