2a71c613de46548457c85e2356c52977e4a22107
DAUDA SHERIFFDEEN DAY <s.dayo36@yahoo.com>
2022-09-16 02:23:45 +0200

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,44 +1,74 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
-*main - program that prints either number
-*or fizz or buzz or fizzBuzz
-*
+*print_triangle - prints a triangle
+*@size:size parameter of triangle
+*Return: returns nothing
+*/
+
+void print_triangle(int size)
+{
+	int inc1, inc2;
+
+	if (size > 0)
+	{
+		for (inc1 = 1; inc1 <= size; inc1++)
+		{
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
+			{
+				putchar(' ');
+			}
+
+			for (inc2 = 0; inc2 < inc1; inc2++)
+			{
+				putchar('#');
+			}
+
+			if (inc1 == size)
+			{
+				continue;
+			}
+
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
+
+
+========================================
+
+TASK 11
+
+#include <stdio.h>
+
+/**
+*main- prints the largest prime factor
+*of a number
 *
 *Return: returns 0
 */
 
 int main(void)
 {
-	int num = 1;
+	long number = 612852475143;
+	int inc;
 
-	while (num++ < 100)
+	while (inc++ < number / 2)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		else if ((num % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((num % 5) == 0)
-		{
-			if (num != 100)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("Buzz");
-			}
-		}
-		else
-		{
-			printf("%d ", num);
-		}
+	if (number % inc == 0)
+	{
+		number /= 2;
+		continue;
 	}
-	printf("\n");
 
+	for (inc = 3; inc < number / 2; inc += 2)
+	{
+		if (number % inc == 0)
+			number /= inc;
+	}
+	}
+	printf("%ld\n", number);
 	return (0);
 }
