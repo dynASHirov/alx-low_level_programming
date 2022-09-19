a3602802c14c98ab97d968ea9a1a1a3ed0c984b4
SHERIFFDEEN <s,dayo36@yahoo.com>
2022-09-19 19:46:14 +0000

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,18 +6,22 @@
  * @s: an input string
  * Return: 0
  */
+
 void rev_string(char *s)
 {
-	int len = 0, i = 0;
-	char aux;
+	int len, i, half;
+	char temp;
 
-	while (s[len] != '\0')
-		len++;
+	for (len = 0; s[len] != '\0'; len++)
+	;
+	i = 0;
+	half = len / 2;
 
-	while (i < len--)
+	while (half--)
 	{
-		aux = s[i];
-		s[i++] = s[len];
-		s[len] = aux;
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
 	}
 }
