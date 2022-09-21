85649059a6d2d5e019d62913c8629d253689af98
SHERIFFDEEN <s,dayo36@yahoo.com>
2022-09-21 20:32:01 +0000

    trcmp

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp -function tha Compares pointers to two strings.
+ * @s1: input string to be compared.
+ * @s2: input string to be compared.
+ *
+ * Return: The difference.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 && *s2) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
