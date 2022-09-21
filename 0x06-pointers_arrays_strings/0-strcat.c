522260c9fd6e08db100879f46895b3de3be7781b
SHERIFFDEEN <s,dayo36@yahoo.com>
2022-09-21 20:14:49 +0000

    srct

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcat -function that concatenates @src to @dest
+ * @src: input string
+ * @dest: input string
+ * Return:pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0;
+	int dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
