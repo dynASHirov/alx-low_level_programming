e33c574c19aa635c6313360874ccd9bcdcf814b2
SHERIFFDEEN <s,dayo36@yahoo.com>
2022-09-22 01:16:55 +0000

    66

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *s)
 
 	while (s[index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
+		while (!(s[index] >= 'a' && s[index] <= 'z'))
 			index++;
 
 		if (s[index - 1] == ' ' ||
