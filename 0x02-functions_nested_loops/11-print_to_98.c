5d670c1d33595236165102f513b7346e77a2deb2
DAUDA SHERIFFDEEN DAYO <s.dayo36@yahoo.com>
2023-04-05 23:00:19 -0700

    task 11 updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,17 +7,24 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d",n);
+			if(n != 98)
+				printf(", ");
+			n++;
+		}
+	}
+	else if (n > 98)
 	{
 		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n", n);
-	}
-
-	else
-	{
-		while (n < 98)
-			printf("%d, ", n++);
-		printf("%d\n", n);
+		{
+			printf("%d", n);
+			if(n != 98)
+				printf(", ");
+			n--;
+		}
 	}
 }
