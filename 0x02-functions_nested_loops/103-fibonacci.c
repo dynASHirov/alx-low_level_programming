11e79413bd5296dd957b5d60d8028b5a8b187c19
DAUDA SHERIFFDEEN DAYO <s.dayo36@yahoo.com>
2023-04-06 23:02:49 -0700

    task 103 modified

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
-int main()
+/*
+ * main - Entry point
+ * return - 0.
+ */
+
+int main(void)
 {
-  long int a = 0;
-  long int b = 1;
-  int c;
-  long int z = 0;
-  long int y;
-  for (c = 0; c < 50; c++)
-    {
-      y = a + b;
-      if ((y % 2) == 0 && y < 4000000)
-        z = z + y;
-     
-      
-      a = b;
-      b = y;
-    }
-  printf("%ld", z);
-  printf("\n");
-  return 0;
+	long int a = 0;
+	long int b = 1;
+	int c;
+	long int z = 0;
+	long int y;
+
+	for (c = 0; c < 50; c++)
+	{
+		y = a + b;
+		if ((y % 2) == 0 && y < 4000000)
+			z = z + y;
+		a = b;
+		b = y;
+	}
+	printf("%ld", z);
+	printf("\n");
+	return (0);
 }
