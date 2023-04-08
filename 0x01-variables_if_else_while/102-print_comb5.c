1fe44f690f815fd80c83b04730bd9b337c44410d
DAUDA SHERIFFDEEN DAYO <s.dayo36@yahoo.com>
2023-04-08 00:05:39 -0700

    task 102 if else

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point;
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
-  int a,b;
-  for (a = 0; a <= 98; a++)
-    {
-      for (b = 1; b <= 99; b++)
-        {
-          if (a != b && a < b)
-          {
-          putchar((a / 10) + '0');
-          putchar((a % 10) + '0');
-          putchar(' ');
-          putchar((b / 10) + '0');
-          putchar((b % 10) + '0');
-          if (a != 98 || b != 99)
-            { 
-            putchar(',');
-            putchar(' ');
-            }  
-          }
-        }
-    }
-  putchar('\n');
-  return 0;
+	int a, b;
+
+	for (a = 0; a <= 98; a++)
+	{
+	for (b = 1; b <= 99; b++)
+	{
+	if (a != b && a < b)
+	{
+		putchar((a / 10) + '0');
+		putchar((a % 10) + '0');
+		putchar(' ');
+		putchar((b / 10) + '0');
+		putchar((b % 10) + '0');
+		if (a != 98 || b != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+	}
 }
