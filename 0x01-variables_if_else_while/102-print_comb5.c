44f4879e5e99f7a41ba412081c3f341787cbd771
DAUDA SHERIFFDEEN DAYO <s.dayo36@yahoo.com>
2023-04-07 23:41:24 -0700

    task 102 if else

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,40 +1,27 @@
 #include <stdio.h>
 
-/**
- *main - Prints 3 combination of numbers
- *
- *Return: Always (Success)
- */
-
 int main(void)
 {
-	int a, b, c, d;
-
-	for (a = '0'; a <= '9'; a++)
-	{
-		for (b = '0'; b <= '9'; b++)
-		{
-			for (c = '0'; c <= '9'; c++)
-			{
-				for (d = '1'; d <= '9'; d++)
-				{
-					if (a <= c && b <= d && b != d)
-						{
-							putchar(a);
-							putchar(b);
-							putchar(' ');
-							putchar(c);
-							putchar(d);
-								if (b != '8' || a != '9' || c != '9' || d != '9')
-							{
-								putchar(',');
-								putchar(' ');
-							}
-						}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+  int a,b;
+  for (a = 0; a <= 98; a++)
+    {
+      for (b = 1; b <= 99; b++)
+        {
+          if (a != b && a < b)
+          {
+          putchar((a / 10) + '0');
+          putchar((a % 10) + '0');
+          putchar(' ');
+          putchar((b / 10) + '0');
+          putchar((b % 10) + '0');
+          if (a != 98 || b != 99)
+            { 
+            putchar(',');
+            putchar(' ');
+            }  
+          }
+        }
+    }
+  putchar('\n');
+  return 0;
 }
