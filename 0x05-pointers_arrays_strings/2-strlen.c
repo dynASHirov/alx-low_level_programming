34903c9c0967414a8119ec6378903e5b1422a5cf
root <root@dynASHIROV>
2023-09-12 07:26:03 +0100

    _sstrlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,10 @@
 #include "main.h"
 
-/**
- * _strlen - function to checks the length of a string
- * @s: input  check
- *
- * Return: int length of the string
- */
-
 int _strlen(char *s)
 {
-	int lgth = 0;
+	int lenght;
+	for(lenght = 0; s[lenght] != '\0'; )
+		lenght++;
 
-	while (s[lgth])
-		lgth++;
-	return (lgth);
+	return (lenght);
 }
