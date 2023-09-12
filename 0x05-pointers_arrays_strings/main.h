b4c28ba97b6b0d11e4f3db5b1d2af15ccc8d1fc1
root <root@dynASHIROV>
2023-09-12 06:06:35 +0100

    reset

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 /**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
+ *main - header file for protoypes
+ *void protoypes (void)
+ *int protoypes (int)
+ */
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
