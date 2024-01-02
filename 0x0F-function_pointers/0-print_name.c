f65f06020bbf1022cdf996b395885e0cff82add6
Sheriffdeen <s.dayo3@yahoo.com>
2024-01-02 09:56:04 +0100

    0 print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,13 @@
 
 /**
  * print_name - a function that prints a name.
- * @_name: pointer to name to print
+ * @name: pointer to name to print
  * @f: function to print name
  * Return: Nothing
  */
-void print_name(char *_name, void (*f)(char *_name))
+void print_name(char *name, void (*f)(char *name))
 {
-	if (_name == NULL || f == NULL)
+	if (name == NULL || f == NULL)
 		return;
-	f(_name);
+	f(name);
 }
